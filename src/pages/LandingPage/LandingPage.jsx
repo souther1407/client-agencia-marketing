d445290b0dde04717ff9f9a4d7791616b95e1461
Eric Casanova <kilbane1407@gmail.com>
2024-06-23 16:00:51 -0300

    implementado nuevos estilos en la landing page, terminado seccion de video y seccion de main ebook

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -8,54 +8,94 @@ import EbookImg from "@assets/Foto Prueba.svg";
 import Link from "../../components/atoms/Link/Link";
 import Input from "../../components/atoms/Input/Input";
 import Footer from "../../components/organisms/Footer/Footer";
+import Icon from "../../components/atoms/Icon/Icon";
+import VideoImg from "@assets/Imagen Video.svg";
+import Mark from "../../components/atoms/Mark/Mark";
 
 const LandingPage = () => {
   return (
     <div className={styles.page}>
+      <div className={styles.wsBtn}>
+        <Icon size={"2.5rem"} type={"wp"} color="var(--primary)" />
+      </div>
       <header className={styles.header}>
         <Nav />
         <section className={styles.banner}>
-          <div className={styles.banner__content}>
-            <Text size="ty" bold="bold" color="primary">
-              AGENCIA DE MARKETING DENTAL
-            </Text>
-            <Text type="title" bold="bold" color="light">
-              Invierte en tu Clínica Dental con InkaDentist
-            </Text>
-            <Text size="sm" color="soft" bold="font-light">
-              Somos expertos en marketing dental. Nuestro objetivo es generar
-              pacientes leales para Clinicas ubicadas en España y Latinoamerica
-            </Text>
+          <div className={styles.contents}>
+            <div className={styles.banner__content}>
+              <Text size="ty" bold="bold" color="primary">
+                AGENCIA DE MARKETING DENTAL
+              </Text>
+              <Text type="title" bold="bold" color="light">
+                Invierte en tu Clínica Dental con InkaDentist
+              </Text>
+              <Text size="sm" color="soft" bold="font-light">
+                Somos expertos en marketing dental. Nuestro objetivo es generar
+                pacientes leales para Clinicas ubicadas en España y
+                Latinoamerica
+              </Text>
 
-            <div className={styles.banner__btns}>
-              <IconTextButton>Solicitar Plan de Marketing</IconTextButton>
+              <div className={styles.banner__btns}>
+                <IconTextButton>Solicitar Plan de Marketing</IconTextButton>
+              </div>
+              <Text size="sm" color="soft" bold="font-light">
+                Solo para Clinicas Dentales que ofrezcan implantes
+              </Text>
             </div>
-            <Text size="sm" color="soft" bold="font-light">
-              Solo para Clinicas Dentales que ofrezcan implantes
-            </Text>
+            <img className={styles.bannerImg} src={BannerImg} />
           </div>
-          <img className={styles.bannerImg} src={BannerImg} />
         </section>
-        <div className={styles.contactBanner}>
-          <div className={styles.desc}></div>
+        <div className={styles.contactBannerSection}>
+          <div className={styles.contactBannerCard}>
+            <div className={styles.desc}>
+              <Text color="primary">¡Pongámonos en contacto!</Text>
+              <Text bold="bold" color="light" type="subtitle">
+                ¡Estamos Listos para Llenar tu Clinica de Pacientes!
+              </Text>
+              <Text color="soft" bold="font-light">
+                Garantizamos ≈10 Nuevos Pacientes cada mes
+              </Text>
+            </div>
+            <IconTextButton icon={"wp"} variant="secondary">
+              ¿Tienes Dudas?
+            </IconTextButton>
+          </div>
         </div>
       </header>
-      <section className={styles.mainEBook}>
-        <div className={styles.mainEBook__card}>
-          <img src={EbookImg} className={styles.mainEBook__img} />
-          <div className={styles.mainEBook__desc}>
-            <Text type="title" bold="bold" textAlign="center">
-              Marketing Dental: La Guia Definitiva
+
+      <section className={styles.videoSection}>
+        <img src={VideoImg} alt="imagen video" className={styles.imgVideo} />
+        <div className={styles.videoSection__desc}>
+          <Text type="title" bold="bold">
+            Nuestra Estrategia de Marketing Dental
+          </Text>
+          <Text bold="font-light">
+            Somos una agencia de marketing especializada en generar pacientes
+            leales para Clinicas Dentales ubicadas en España y Latinoamerica
+          </Text>
+          <div className={styles.btn}>
+            <IconTextButton>Solicita una reunión gratis</IconTextButton>
+            <Text bold="font-light" size="ty">
+              Discutamos el crecimiento de tu práctica dental
             </Text>
-            <Text size="sm" bold="light" textAlign="center">
-              Transforma tu clínica dental con nuestros ebooks, redactados por
-              líderes en marketing digital. Transforma tu clínica dental con
-              nuestros ebooks, redactados por líderes en marketing digital.
-            </Text>
-            <Link>
-              <Text color="link">Descargar la Guia</Text>
-            </Link>
           </div>
+        </div>
+      </section>
+      <section className={styles.mainEBookSection}>
+        <div className={styles.mainEBook__card}>
+          <div className={styles.mainEBook__desc}>
+            <Text type="subtitle" bold="bold" color="light">
+              <Mark color="primary">Marketing Dental:</Mark> La Guia Definitiva
+            </Text>
+            <Text size="sm" bold="font-light" color="soft">
+              Transforma tu clínica dental con nuestros ebook, redactados por
+              nuestro director de Marketing.
+            </Text>
+            <div className={styles.btnDownload}>
+              <IconTextButton>Descarga Gratis</IconTextButton>
+            </div>
+          </div>
+          <img src={EbookImg} className={styles.mainEBook__img} />
         </div>
       </section>
       <section className={styles.form}>
