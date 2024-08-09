cfd1a8e452e14ea483a0b73b749d8609a03ec5c8
Eric Casanova <kilbane1407@gmail.com>
2024-08-09 18:54:14 -0300

    implementado footer y prefooter en pantalla descargar ebook

--- src/pages/DownloadEBook/DownloadEBook.jsx
+++ src/pages/DownloadEBook/DownloadEBook.jsx
@@ -6,7 +6,11 @@ import IconTextButton from "../../components/molecules/IconTextButton/IconTextBu
 import Modal from "../../components/molecules/Modal/Modal";
 import DownloadForm from "./components/DownloadForm/DownloadForm";
 import Logo from "../../components/atoms/Logo/Logo";
+import Icon from "../../components/atoms/Icon/Icon";
 import EBookImg from "@assets/Foto Prueba.svg";
+import EbookPortada from "@assets/ImgPortadaEbook.svg";
+import Link from "../../components/atoms/Link/Link";
+
 const DownloadEBook = () => {
   const [modalOpened, setModalOpened] = useState(false);
   return (
@@ -41,19 +45,63 @@ const DownloadEBook = () => {
             <Text bold="bold" type="subtitle">
               Adentro aprenderás:
             </Text>
-            <Text bold="font-light">
-              Panorama de las ventas por chat para el 2023
-            </Text>
-            <Text bold="font-light">
-              Cuáles son las industrias que más venden por chat aprendiendo a
-              vender por medio de Facebook.
-            </Text>
-            <Text bold="font-light">
-              Aprendiendo a vender por medio de WhatsApp, Telegram e Instagram
-            </Text>
+            <div className={styles.line}>
+              <Icon type={"check"} color="var(--primary-dark)" size={"1rem"} />{" "}
+              <Text bold="font-light">
+                Panorama de las ventas por chat para el 2023
+              </Text>
+            </div>
+            <div className={styles.line}>
+              <Icon type={"check"} color="var(--primary-dark)" size={"1rem"} />{" "}
+              <Text bold="font-light">
+                Cuáles son las industrias que más venden por chat aprendiendo a
+                vender por medio de Facebook.
+              </Text>
+            </div>
+            <div className={styles.line}>
+              <Icon type={"check"} color="var(--primary-dark)" size={"1rem"} />{" "}
+              <Text bold="font-light">
+                Aprendiendo a vender por medio de WhatsApp, Telegram e Instagram
+              </Text>
+            </div>
           </div>
+          <img src={EbookPortada} className={styles.imgPortada} />
         </div>
       </main>
+      <section className={styles.prefooter}>
+        <Text color="light" type="title">
+          Obten tu Guia Gratis
+        </Text>
+        <div className={styles.downloadBtn}>
+          <IconTextButton size="100%" textProps={{ size: "lg" }}>
+            Descargar Ahora
+          </IconTextButton>
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <Text color="light" bold="font-light">
+          Copyright © {new Date().getFullYear()}, HackDental.com
+        </Text>
+        <div className={styles.links}>
+          <Link to={"#"}>
+            <Text color="soft-light" size="ty" bold="font-light">
+              Politica de Privacidad
+            </Text>
+          </Link>
+          <hr className={styles.separator} />
+          <Link to={"#"}>
+            <Text color="soft-light" size="ty" bold="font-light">
+              Politica de Privacidad
+            </Text>{" "}
+          </Link>
+          <hr className={styles.separator} />
+          <Link to={"#"}>
+            <Text color="soft-light" size="ty" bold="font-light">
+              Politica de Privacidad
+            </Text>
+          </Link>
+        </div>
+      </footer>
       <Modal isOpen={modalOpened} onClose={() => setModalOpened(false)}>
         <DownloadForm modalOpened={modalOpened} />
       </Modal>
