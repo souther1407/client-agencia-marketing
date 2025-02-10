38f7e4be75e3b16d2a86140e2db840c02e4af5e0
Eric Casanova <kilbane1407@gmail.com>
2025-02-09 21:04:47 -0300

    cambios 103

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -253,9 +253,10 @@ const LandingPage = () => {
               fontFamily="font-cuarternary"
               s={{ lineHeight: "1.4" }}
             >
-              5 Razones por las que pacientes{" "}
-              <Mark color="primary-bg-triangular">no eligen</Mark>
-              <br /> tu consultorio
+              5 Razones por las que <br />
+              pacientes <Mark color="primary-bg-triangular">no eligen</Mark>
+              <br />
+              tu consultorio
             </Text>
 
             <div className={styles.quieroHacerCrecerBtn}>
