52a0b2cdba5d2909cd092d48e132798b1e00c6d4
Eric Casanova <kilbane1407@gmail.com>
2025-02-18 19:59:58 -0300

    cambios 235

--- src/components/organisms/PreefooterSimple/PreefooterSimple.jsx
+++ src/components/organisms/PreefooterSimple/PreefooterSimple.jsx
@@ -5,13 +5,17 @@ import styles from "./styles.module.css";
 const PreefooterSimple = () => {
   return (
     <div className={styles.preefooter}>
-      <Text type="subtitle" color="dark" bold="semibold">
-        Impulsemos tu Clínica, Juntos
-      </Text>
-      <Text bold="font-light" color="dark">
-        Aprende como Hack Dental puede ayudarte con una auditoría 100% gratuita
-      </Text>
-      <IconTextButton size="300px">Pedir auditoría</IconTextButton>
+      <div className={styles.desc}>
+        <Text type="subtitle" color="dark" bold="semibold">
+          Impulsemos tu Clínica, Juntos
+        </Text>
+        <Text bold="font-light" color="dark">
+          Te ayudamos con una auditoría 100% gratuita
+        </Text>
+      </div>
+      <div className={styles.pedirAuditoria}>
+        <IconTextButton size="300px">Pedir auditoría</IconTextButton>
+      </div>
     </div>
   );
 };
