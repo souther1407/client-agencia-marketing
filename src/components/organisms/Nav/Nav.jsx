77dc53122f55b2bf3d3415dc6fe6a1ab7c893829
Eric Casanova <kilbane1407@gmail.com>
2025-02-18 20:40:36 -0300

    cambios 253

--- src/components/organisms/Nav/Nav.jsx
+++ src/components/organisms/Nav/Nav.jsx
@@ -251,7 +251,7 @@ const Nav = ({ hideTopMenu = false, hideBottomMenu = false }) => {
         {hideTopMenu || isTopMenuHidden || (
           <div className={styles.topMenu} onClick={() => navigate(OUR_PROGRAM)}>
             <span className={styles.whiteMark}>
-              <Text s={{ fontSize: "12px" }} bold="semibold" color="black">
+              <Text s={{ fontSize: "11px" }} bold="semibold" color="black">
                 NUEVO
               </Text>
             </span>
